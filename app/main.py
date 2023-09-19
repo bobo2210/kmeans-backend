88d3f23e9609a862bb675eb25f1ec7503f457b63
FynnEmmrich <143699785+FynnEmmrich@users.noreply.github.com>
2023-09-19 21:43:30 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,9 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from fastapi import FastAPI
 
-CFG_PORT = 5000
-app = Flask(__name__)
-api = Api(app)
+# Erstelle eine FastAPI-Instanz
+app = FastAPI()
 
-class HelloWorld(Resource):
-    """
-    Test API Endpoint
-    """
-    def get(self):
-        """
-        Getter for hello world
-        """
-
-        data = {"data": "test2"}
-
-        return data
-
-api.add_resource(HelloWorld,'/hello')
-
-if __name__=='__main__':
-    app.run(host="0.0.0.0", port=CFG_PORT)
+# Definiere eine Route für die Wurzel-URL ("/"), die "Hello, World!" zurückgibt
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
