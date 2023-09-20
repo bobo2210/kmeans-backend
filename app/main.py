d0b7d37ed74a8ad67a18b86a4d03cca3669d93e2
FynnEmmrich <143699785+FynnEmmrich@users.noreply.github.com>
2023-09-20 15:04:35 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,24 +17,5 @@ def root():
     """
     return {"message": "Hello, World!"}
 
-@app.post("/uploadcsv/")
-async def upload_csv(file: UploadFile):
-    """
-    Uploads a CSV file and returns its contents as a DataFrame.
-
-    Args:
-        file (UploadFile): The uploaded CSV file.
-
-    Returns:
-        dict: A dictionary containing the DataFrame with the CSV data.
-              If the uploaded file is not a CSV, an error message is returned.
-    """
-    #if file.filename.endswith(".csv"):
-    # CSV-Datei direkt mit pandas einlesen
-    dataframe = pd.read_csv(file.file)
-    return {"dataframe": dataframe.to_dict()}  # Convert the DataFrame to a dictionary
-    #return {"error": "Die hochgeladene Datei ist keine CSV-Datei."}
-
-
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=5000)
