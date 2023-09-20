476c2036976254fe8c7fbe1d2184478886b96bd6
bobo2210 <mar.schuering@gmail.com>
2023-09-20 07:55:54 +0200

    update test_api

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from app.main import app, api, HelloWorld, CFG_PORT
 def test_setup():
     """
     Tests the setup of the API endpoint
-    """    
+    """
     assert app is not None
     assert api is not None
     assert CFG_PORT == 5000 # required to properly setup preceeding deployment step
@@ -16,7 +16,7 @@ def test_setup():
 def test_endpoint():
     """
     Tests the endpoint contract
-    """    
+    """
     hello_world = HelloWorld()
     res = hello_world.get()
     assert res['data'] is not None
