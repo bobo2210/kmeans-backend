1a7519cc229fcb8af599875c621fb4bfc63a0d4f
FynnEmmrich <143699785+FynnEmmrich@users.noreply.github.com>
2023-09-20 13:59:48 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,13 @@
 """Module providing Function to run Webserver/API"""
-from flask import Flask
-from flask_restful import Api, Resource
+from fastapi import FastAPI
 
-CFG_PORT = 5000
-app = Flask(__name__)
-api = Api(app)
+app = FastAPI()
 
-class HelloWorld(Resource):
-    """
-    Test API Endpoint
-    """
-    def get(self):
-        """
-        Getter for hello world
-        """
-
-        data = {"data": "test4"}
-
-        return data
-
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
 api.add_resource(HelloWorld,'/hello')
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=CFG_PORT)
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
