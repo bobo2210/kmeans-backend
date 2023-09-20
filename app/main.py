e5089e7c6b7b3a6a183324a2a2cdfcc015f09ba9
FynnEmmrich <143699785+FynnEmmrich@users.noreply.github.com>
2023-09-20 14:08:11 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 """Module providing Function to run Webserver/API"""
 from fastapi import FastAPI
-from uvicorn import uvicorn
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def root():
     return {"message": "Hello, World!"}
 
-
 if __name__=='__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
