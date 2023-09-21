e6e3d8a52a426a1b4d65e8f0eb539a3bb8cedcc0
FynnEmmrich <143699785+FynnEmmrich@users.noreply.github.com>
2023-09-21 09:45:03 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,29 +2,23 @@
 from fastapi import FastAPI, UploadFile
 import uvicorn
 import pandas as pd
+import asyncio
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    """
-    Handle the root endpoint.
+# Dictionary to store tasks, including status and results
+tasks = {}
 
-    This function returns a JSON message with a greeting.
 
-    Returns:
-        dict: A dictionary containing a greeting message.
+@app.post("/kmeans/")
+async def kmeans(file: UploadFile, num_clusters int = 2):
     """
-    return {"message": "Hello, World!"}
-
-@app.post("/uploadcsv/")
-async def upload_csv(file: UploadFile):
-    """
-    Uploads a CSV file and returns its contents as a DataFrame.
+    Uploads a CSV file, performs k-means and returns an array with the clusters 
 
     Args:
         file (UploadFile): The uploaded CSV file.
-
+        num_clusters (int): The number of clusters default = 2
+        
     Returns:
         dict: A dictionary containing the DataFrame with the CSV data.
               If the uploaded file is not a CSV, an error message is returned.
@@ -32,8 +26,74 @@ async def upload_csv(file: UploadFile):
     if file.filename.endswith(".csv"):
         # Read the CSV file directly with pandas
         dataframe = pd.read_csv(file.file)
-        return {"dataframe": dataframe.to_dict()}
+        # Erstelle eine eindeutige Task-ID
+        task_id = len(tasks) + 1
+        # Initialize the task with a "processing" status and an empty results list
+        tasks[task_id] = {"status": "processing", "results": []}
+        asyncio.create_task(run_kmeans(dataf))
+        return {"TaskID": task_id}
     return {"error": "Die hochgeladene Datei ist keine CSV-Datei."}
+
+
+async def run_kmeans(dataframe, num_clusters):
+    """
+    Uploads a CSV file, performs k-means and returns an array with the clusters 
+
+    Args:
+        file (UploadFile): The uploaded CSV file.
+        num_clusters (int): The number of clusters default = 2
+        
+    Returns:
+        dict: A dictionary containing the DataFrame with the CSV data.
+              If the uploaded file is not a CSV, an error message is returned.
+    """
+    # Instanciate sklearn's k-means using num_clusters clusters
+    kmeans = KMeans(n_clusters=2, n_init='auto', verbose = 2)
+
+    # execute k-means algorithm
+    kmeans.fit(df.values)
+    # Update the task with the "completed" status and the results
+    tasks[task_id]["status"] = "completed"
+    tasks[task_id]["results"] = kmeans.labels_
+
+
+@app.get("/kmeans/status/{task_id}")
+async def get_task_status(task_id: int):
+    """
+    Uploads a CSV file, performs k-means and returns an array with the clusters 
+
+    Args:
+        file (UploadFile): The uploaded CSV file.
+        num_clusters (int): The number of clusters default = 2
+        
+    Returns:
+        
+    """
+    if task_id not in tasks:
+        raise HTTPException(status_code=404, detail="Task not found")
+        
+    task_status = tasks[task_id]["status"]
+    return {"status": task_status}
+    
+@app.get("/kmeans/result/{task_id}")
+async def get_task_result(task_id: int):
+    """
+    gets the results of the kmeans method
+
+    Args:
+        taskid, the id of the regarded task
+        
+    Returns:
+        
+    """
+    if task_id not in tasks:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    task_result = tasks[task_id]["results"]
+    if not task_result:
+        raise HTTPException(status_code=404, detail="Task result not available yet")
+
+    return task_result
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=5000)
