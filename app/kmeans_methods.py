b1162458628adbb426f885756b364eb20f299f46
FynnEmmrich <fynn.emmrich@web.de>
2023-09-22 09:10:38 +0200

    Neue file für kmeans, json Datei einlesen

---
+++ app/kmeans_methods.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+
+async def run_kmeans_one_k(dataframe, num_clusters, task_id, tasks):
+    """
+    Uploads a CSV file, performs k-means, and returns an array with the clusters 
+
+    Args:
+        dataframe (pd.DataFrame): The uploaded CSV data.
+        num_clusters (int): The number of clusters, default = 2
+        task_id (int): The task ID
+        
+    Returns:
+        dict: A dictionary with the DataFrame with the CSV data.
+              If the uploaded file is not a CSV, an error message is returned.
+    """
+    # Instantiate sklearn's k-means using num_clusters clusters
+    kmeans = KMeans(n_clusters=num_clusters, n_init='auto', verbose=2)
+
+    # execute k-means algorithm
+    kmeans.fit(dataframe.values)
+    # Update the task with the "completed" status and the results
+    tasks[task_id]["status"] = "completed"
+    tasks[task_id]["results"] = kmeans.labels_
+
+    # Return the cluster labels as a Series
+    return pd.Series(kmeans.labels_)
--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 """Module providing Function to run Webserver/API """
 import asyncio
 import json
+import logging
 from fastapi import FastAPI, UploadFile
 from fastapi.exceptions import HTTPException
 import uvicorn
 import pandas as pd
-from sklearn.cluster import KMeans
+from kmeans_methods import run_kmeans_one_k
 
 app = FastAPI()
+
+logger = logging.getLogger(__name__)
 
 # Dictionary to store tasks, including status and results
 tasks = {}
@@ -16,58 +19,40 @@ tasks = {}
 @app.post("/kmeans/")
 async def kmeans_start(file: UploadFile, num_clusters: int = 2):
     """
-    Uploads a CSV file, performs k-means, and returns an array with the clusters 
+    Uploads a json file, performs k-means, and returns the id of the task
 
     Args:
-        file (UploadFile): The uploaded CSV file.
+        file (UploadFile): The uploaded json file.
         num_clusters (int): The number of clusters, default = 2
         
     Returns:
-        dict: A dictionary containing the DataFrame with the CSV data.
-              If the uploaded file is not a CSV, an error message is returned.
+        dict: The Id of the task
+              If the uploaded file is not a json, an error message is returned.
     """
     if file.filename.endswith(".json"):
         #json Datei öffnen
         with file.file as json_file:
             data = json.load(json_file)
-        # Zugriff auf die Parameter für K-Means
+
+        #Zugriff auf die Parameter für K-Means
         #kmeans_parameters = data["kmeans_parameters"]
         #k_value = kmeans_parameters["k"]
         #max_iterations = kmeans_parameters["max_iterations"]
         #tolerance = kmeans_parameters["tolerance"]
+
         # Zugriff auf die Datenpunkte
-        data_points = data["data_points"]
+        data_points = data.get("data_points", [])
         # Erstellen eines  Pandas DataFrame
         dataframe = pd.DataFrame(data_points)
         # Create a unique task ID
         task_id = len(tasks) + 1
         # Initialize the task with a "processing" status and an empty results list
         tasks[task_id] = {"status": "processing", "results": []}
-        asyncio.create_task(run_kmeans_one_k(dataframe, num_clusters, task_id))
+
+        asyncio.create_task(run_kmeans_one_k(dataframe, num_clusters, task_id, tasks))
+
         return {"TaskID": task_id}
     return {"error": "Die hochgeladene Datei ist keine CSV-Datei."}
-
-async def run_kmeans_one_k(dataframe, num_clusters, task_id):
-    """
-    Uploads a CSV file, performs k-means, and returns an array with the clusters 
-
-    Args:
-        dataframe (pd.DataFrame): The uploaded CSV data.
-        num_clusters (int): The number of clusters, default = 2
-        task_id (int): The task ID
-        
-    Returns:
-        dict: A dictionary with the DataFrame with the CSV data.
-              If the uploaded file is not a CSV, an error message is returned.
-    """
-    # Instantiate sklearn's k-means using num_clusters clusters
-    kmeans = KMeans(n_clusters=num_clusters, n_init='auto', verbose=2)
-
-    # execute k-means algorithm
-    kmeans.fit(dataframe.values)
-    # Update the task with the "completed" status and the results
-    tasks[task_id]["status"] = "completed"
-    tasks[task_id]["results"] = kmeans.labels_
 
 
 @app.get("/kmeans/status/{task_id}")
@@ -99,12 +84,12 @@ async def get_task_result(task_id: int):
     """
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="Task not found")
-
+    task_status = tasks[task_id]["status"]
     task_result = tasks[task_id]["results"]
-    if not task_result:
-        raise HTTPException(status_code=404, detail="Task result not available yet")
+    if task_status != "completed":
+        raise HTTPException(status_code=400, detail="Task result not available yet")
 
-    return {"result": task_result}
+    return {"result": task_result.tolist()}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=5000)
