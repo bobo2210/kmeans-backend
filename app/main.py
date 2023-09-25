c25bec654164a81a96b79ceb28f76f2c8da2db21
FynnEmmrich <143699785+FynnEmmrich@users.noreply.github.com>
2023-09-25 10:31:39 +0200

    Linter

--- app/main.py
+++ app/main.py
@@ -38,7 +38,8 @@ async def kmeans_start(file: UploadFile):
         #Zugriff auf die Parameter für K-Means
         kmeans_parameters = data["kmeans_parameters"]
         centroids_start_json = data["centroids"]
-        centroids_start = np.array([[centroid["x"], centroid["y"]] for centroid in centroids_start_json])
+        centroids_start = np.array([[centroid["x"], 
+                                     centroid["y"]] for centroid in centroids_start_json])
 
         # Zugriff auf die Datenpunkte
         data_points = data.get("data_points", [])
@@ -49,7 +50,11 @@ async def kmeans_start(file: UploadFile):
         # Initialize the task with a "processing" status and an empty results list
         tasks[task_id] = {"status": "processing", "results": [], "message": ""}
 
-        asyncio.create_task(run_kmeans_one_k(dataframe, task_id, tasks, kmeans_parameters, centroids_start))
+        asyncio.create_task(run_kmeans_one_k(dataframe, 
+                                             task_id, 
+                                             tasks, 
+                                             kmeans_parameters, 
+                                             centroids_start))
 
         return {"TaskID": task_id}
     return {"error": "Die hochgeladene Datei ist keine json-Datei."}
