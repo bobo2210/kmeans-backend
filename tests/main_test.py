28f6e9905f36c264ad704d583ee7cade5fbc6460
bobo2210 <mar.schuering@gmail.com>
2023-09-26 13:57:43 +0200

    api test update

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,10 +1,10 @@
-"""
-Unit tests for Flask app
-"""
-from app.main import CFG_PORT
+"""Main test"""
+from fastapi.testclient import TestClient
+from app.main import app
 
-def test_port() -> None:
-    """
-    Tests the setup of the API endpoint
-    """
-    assert CFG_PORT == 5000
+client = TestClient(app)
+
+def test_read_main():
+    """Test API"""
+    response = client.get("/kmeans/status/2")
+    assert response.json() == {"detail": "Task not found"}
