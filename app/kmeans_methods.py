17d01b22ceff2f3de461c020e576e9b7765255d4
FynnEmmrich <fynn.emmrich@web.de>
2023-09-26 21:55:26 +0200

    Fehler

--- app/kmeans_methods.py
+++ app/kmeans_methods.py
@@ -17,9 +17,10 @@ async def run_kmeans_one_k(dataframe, task_id, tasks, kmeans_parameters, centroi
         dict: A dictionary with the DataFrame with the CSV data.
               If the uploaded file is not a CSV, an error message is returned.
     """
-    #Dateicheck einfügen
+    #Dateicheck einfï¿½gen
     dataframe_clean = data_check(dataframe)
     #get the different params
+
     if "k" in kmeans_parameters: 
         k_value = kmeans_parameters["k"]
     else:
@@ -67,6 +68,39 @@ async def run_kmeans_one_k(dataframe, task_id, tasks, kmeans_parameters, centroi
         # Instantiate sklearn's k-means using num_clusters clusters
         kmeans = KMeans(n_clusters=k_value, init=initialisation, n_init=number_runs, max_iter=maximum_iter, tol=tolerance, algorithm=used_algorithm, verbose=2)
     elif initialisation == "centroids":
+
+    k_value = kmeans_parameters["k"]
+    number_runs = kmeans_parameters["number_runs"]
+    maximum_iter = kmeans_parameters["max_iterations"]
+    tolerance = kmeans_parameters["tolerance"]
+    #distance_nominal = kmeans_parameters["distance_nominal"]
+    #distance_cardinal = kmeans_parameters["distance_cardinal"]
+    initialisation = kmeans_parameters["init"]
+    used_algorithm = kmeans_parameters["algorithm"]
+    error_message = ""
+    if not isinstance(number_runs, int):
+        error_message += "The Number of runs has to be an integer. "
+    if not isinstance(tolerance, float):
+        error_message += "The tolerance has to be a float value. "
+    if not isinstance(maximum_iter, int):
+        error_message += "The maximal number of iterations has to be an integer. "
+    if not isinstance(number_runs, int) and number_runs != 'auto':
+        error_message += "The number of kmeans-runs has to be an integer. "
+    if k_value > len(dataframe) or not isinstance(k_value, int):
+        error_message += ("The k-value has to be an integer"
+                          " and smaller than the number of datapoints. ")
+    if initialisation in ("k-means++","random"):
+        # Instantiate sklearn's k-means using num_clusters clusters
+        kmeans = KMeans(
+            n_clusters=k_value,
+            init=initialisation,
+            n_init=number_runs,
+            max_iter=maximum_iter,
+            tol=tolerance,
+            algorithm=used_algorithm,
+            verbose=2)
+    elif initialisation == "cluster":
+
         # Instantiate sklearn's k-means using num_clusters clusters
         kmeans = KMeans(
             n_clusters=k_value,
@@ -82,6 +116,7 @@ async def run_kmeans_one_k(dataframe, task_id, tasks, kmeans_parameters, centroi
                           " of the initial centroid positions. ")
     if error_message != "":
         tasks[task_id]["status"] = "Bad Request"
+
         tasks[task_id]["message"] = tasks[task_id]["message"] + "The parameter init has to be k-means++, random or cluster in combination with a specification of the initial centroid positions. "
     if tasks[task_id]["status"] != "Bad Request":
         # execute k-means algorithm
@@ -90,3 +125,12 @@ async def run_kmeans_one_k(dataframe, task_id, tasks, kmeans_parameters, centroi
         tasks[task_id]["status"] = "completed"
         tasks[task_id]["results"] = kmeans.labels_
         tasks[task_id]["centroid_positions"] = kmeans.cluster_centers_
+
+        tasks[task_id]["message"] = error_message
+        return
+
+    # execute k-means algorithm
+    kmeans.fit(dataframe.values)
+    # Update the task with the "completed" status and the results
+    tasks[task_id]["status"] = "completed"
+    tasks[task_id]["results"] = kmeans.labels_
