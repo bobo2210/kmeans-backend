784dd1e6227c2d60196f48d8756094cc1ae24d3a
FelixFaw <143699922+FelixFaw@users.noreply.github.com>
2023-10-11 23:40:32 +0200

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -103,7 +103,7 @@ def elbow_to_json(Kmin, Kmax, elbow):
     data = {}  
 
     for k in range(Kmin, Kmax + 1):
-    data[k] = elbow[k - Kmin]
+        data[k] = elbow[k - Kmin]
 
     # Konvertiere das Dictionary in einen  JSON-String
     json_string = json.dumps(data, indent=4)
