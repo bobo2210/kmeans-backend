66af69d5afab5eb5fe8ee65531b46cba946bcad0
FynnEmmrich <fynn.emmrich@web.de>
2023-10-11 23:38:04 +0200

    indent hinzugefügt

--- app/utils.py
+++ app/utils.py
@@ -100,7 +100,7 @@ def elbow_to_json(Kmin, Kmax, elbow):
         function to store elbow data in json string
     """
     for k in range(Kmin, Kmax + 1):
-    data[k] = elbow[k - Kmin]
+        data[k] = elbow[k - Kmin]
 
     # Konvertiere das Dictionary in einen  JSON-String
     json_string = json.dumps(data, indent=4)
