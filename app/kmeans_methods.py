2ce7c290b58b7991b44952661616eecf23db2388
FelixFaw <143699922+FelixFaw@users.noreply.github.com>
2023-10-11 19:38:59 +0200

    Update kmeans_methods.py

--- app/kmeans_methods.py
+++ app/kmeans_methods.py
@@ -28,7 +28,8 @@ def run_kmeans_one_k(dataframe,
               If the uploaded file is not a CSV, an error message is returned.
     """
     #Dateicheck einfuegen
-    #dataframe_clean = main.data_check(dataframe)
+    dataframe = main.data_check(dataframe, tasks, taskid)
+
     kmeans = None
     if initialisation in ("k-means++","random"):
         # Instantiate sklearn's k-means using num_clusters clusters
